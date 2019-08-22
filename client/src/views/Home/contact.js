0d5329549729c7f7c2ab73c3580aa66bebeb7d2f
traveltek-dagosti <davidea@traveltek.net>
2019-08-22 19:10:20 +0100

    Update Views

--- client/src/views/Home/contact.js
+++ client/src/views/Home/contact.js
@@ -1,10 +1,78 @@
 import React from "react";
 
 export default function HomeContact() {
-	return (
-		<div id="contact" className="section">
-			<h2>Contact</h2>
-			<div className="empty" />
-		</div>
-	);
+  return (
+    <div id="contact" className="section padding-v-60 min-fullheight">
+      {/*-- BG Map --*/}
+      <div
+        id="google-map"
+        className="bg-image"
+        data-latitude="40.758895"
+        data-longitude="-73.985131"
+        data-style="light"
+      />
+
+      {/*-- Contact Box --*/}
+      <div
+        className="contact-box bg-secondary dark animated"
+        data-animation="flipInY"
+      >
+        <h1>
+          Don’t hesitate to <strong>contact me</strong>!
+        </h1>
+        {/*-- Contact List --*/}
+        <ul className="list-unstyled list-unstyled-icons">
+          <li>
+            <i className="inline-icon icon-before-and-after text-primary fa fa-map-marker" />
+            15 Nowhere Lane,
+            <br />
+            Outta Sight,
+            <br />
+            State KA123AB, Scotland
+          </li>
+          <li>
+            <i className="inline-icon icon-before-and-after text-primary fa fa-comment" />
+            <a href="/">davide.a@davideagosti.info</a>
+          </li>
+          <li>
+            <i className="inline-icon icon-before-and-after text-primary fa fa-phone" />
+            +0(44)6 89764536
+          </li>
+        </ul>
+        <a href="/" className="btn btn-primary" data-target="messenger">
+          <span>Use Contact Form</span>
+          <i className="ti-email" />
+        </a>
+        <h5 className="margin-t-60">Check my social media!</h5>
+        <ul className="list-inline">
+          <li>
+            <a href="/" className="icon icon-circle icon-xs icon-facebook">
+              <i className="fa fa-facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="/" className="icon icon-circle icon-xs icon-twitter">
+              <i className="fa fa-twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="/" className="icon icon-circle icon-xs icon-google-plus">
+              <i className="fa fa-twitter" />
+            </a>
+          </li>
+        </ul>
+        <h5>Like me on Facebook</h5>
+        {/*-- FB Like --*/}
+        <div
+          className="fb-like"
+          data-href="http://www.facebook.com/davideagosti"
+          data-layout="button_count"
+          data-action="like"
+          data-show-faces="false"
+          data-share="false"
+        />
+      </div>
+    </div>
+  );
 }
+
--- client/src/views/Home/header.js
+++ client/src/views/Home/header.js
@@ -7,105 +7,118 @@ import {
 	scroller
 } from "react-scroll";
 
-export default function HomeHeader() {
+export default function HomeHeader({ profiles, loading }) {
 	return (
 		<Fragment>
-			{/* -- Logo -- */}
-			<div className="logo bg-primary">
-				<h1 className="name">
-					Davide <strong>Agosti</strong>
-				</h1>
-				<span className="caption text-secondary">
-					Front End / Web developer
-				</span>
-			</div>
+			{(profiles === null || loading) && (
+				<div>
+					<h2>loading...</h2>
+				</div>
+			)}
+			{profiles && (
+				<Fragment>
+					{/* -- Logo -- */}
+					<div className="logo bg-primary">
+						<h1 className="name">
+							Davide <strong>Agosti</strong>
+						</h1>
+						<span className="caption text-secondary">
+							FrontEnd / Web developer
+						</span>
+					</div>
 
-			{/* -- Navigation -- */}
-			<nav id="main-menu">
-				<ul className="nav nav-vertical">
-					<li>
-						<Link
-							activeClass="active"
-							to="start"
-							spy={true}
-							smooth={true}
-							duration={600}
-						>
-							<span>Home</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							activeClass="active"
-							to="resume"
-							spy={true}
-							smooth={true}
-							duration={600}
-							offset={0}
-						>
-							<span>About</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							activeClass="active"
-							to="portfolio"
-							spy={true}
-							smooth={true}
-							duration={600}
-							offset={0}
-						>
-							<span>Portfolio</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							activeClass="active"
-							to="posts"
-							spy={true}
-							smooth={true}
-							duration={600}
-							offset={0}
-						>
-							<span>Blog</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							activeClass="active"
-							to="contact"
-							spy={true}
-							smooth={true}
-							duration={600}
-							offset={0}
-						>
-							<span>Contact</span>
-						</Link>
-					</li>
-				</ul>
-			</nav>
+					{/* -- Navigation -- */}
+					<nav id="main-menu">
+						<ul className="nav nav-vertical">
+							<li>
+								<Link
+									activeclassName="active"
+									to="start"
+									spy={true}
+									smooth={true}
+									duration={600}
+								>
+									<span>Home</span>
+								</Link>
+							</li>
+							<li>
+								<Link
+									activeclassName="active"
+									to="resume"
+									spy={true}
+									smooth={true}
+									duration={600}
+									offset={0}
+								>
+									<span>About</span>
+								</Link>
+							</li>
+							<li>
+								<Link
+									activeclassName="active"
+									to="portfolio"
+									spy={true}
+									smooth={true}
+									duration={600}
+									offset={0}
+								>
+									<span>Portfolio</span>
+								</Link>
+							</li>
+							<li>
+								<Link
+									activeclassName="active"
+									to="posts"
+									spy={true}
+									smooth={true}
+									duration={600}
+									offset={0}
+								>
+									<span>Blog</span>
+								</Link>
+							</li>
+							<li>
+								<Link
+									activeclassName="active"
+									to="contact"
+									spy={true}
+									smooth={true}
+									duration={600}
+									offset={0}
+								>
+									<span>Contact</span>
+								</Link>
+							</li>
+						</ul>
+					</nav>
 
-			{/* -- Social Media -- */}
-			<div className="social-media">
-				<strong className="text-sm hidden-md">Check my social media!</strong>
-				<ul className="list-inline margin-t-10">
-					<li>
-						<a href="/" className="icon icon-xs">
-							<i className="fa fa-facebook text-muted" />
-						</a>
-					</li>
-					<li>
-						<a href="/" className="icon icon-xs">
-							<i className="fa fa-twitter text-muted" />
-						</a>
-					</li>
-					<li>
-						<a href="/" className="icon icon-xs">
-							<i className="fa fa-google-plus text-muted" />
-						</a>
-					</li>
-				</ul>
-			</div>
+					{/* -- Social Media -- */}
+					<div className="social-media">
+						<strong className="text-sm hidden-md">
+							Check my social media!
+						</strong>
+						{profiles.map(profile => (
+							<ul key="social-1" className="list-inline margin-t-10">
+								<li>
+									<a href={profile.social.facebook} className="icon icon-xs">
+										<i className="fa fa-facebook text-muted" />
+									</a>
+								</li>
+								<li>
+									<a href={profile.social.twitter} className="icon icon-xs">
+										<i className="fa fa-twitter text-muted" />
+									</a>
+								</li>
+								<li>
+									<a href={profile.social.googlePlus} className="icon icon-xs">
+										<i className="fa fa-google-plus text-muted" />
+									</a>
+								</li>
+							</ul>
+						))}
+					</div>
+				</Fragment>
+			)}
 		</Fragment>
 	);
 }
