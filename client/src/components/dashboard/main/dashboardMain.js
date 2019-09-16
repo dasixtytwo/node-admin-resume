7913913c598702c1e338227d3762b8adf518c712
Davide Agosti <davide.a@davideagosti.info>
2019-09-16 20:16:47 +0100

    Update Dashboard component

--- client/src/components/dashboard/main/dashboardMain.js
+++ client/src/components/dashboard/main/dashboardMain.js
@@ -1,6 +1,6 @@
-import React, { Fragment, useState } from 'react';
-import DashboardAside from 'components/dashboard/aside/dashboardAside';
-import { Layout, Breadcrumb, Icon } from 'antd';
+import React, { Fragment, useState } from "react";
+import DashboardAside from "components/dashboard/aside/dashboardAside";
+import { Layout, Breadcrumb, Icon } from "antd";
 
 const { Header, Content, Footer } = Layout;
 
@@ -9,35 +9,35 @@ export default function DashboardHeader() {
 	const [stateCollapsed, setStateCollapsed] = useState(INITIAL_STATE);
 
 	const toggle = () => {
-		const collapsed = 'collapsed';
+		const collapsed = "collapsed";
 		setStateCollapsed({ [collapsed]: !stateCollapsed[collapsed] });
 	};
 
 	return (
 		<Fragment>
-			<DashboardAside collapsed={stateCollapsed} />
+			<DashboardAside collapsed={stateCollapsed.collapsed} />
 			<Layout>
-				<Header style={{ background: '#fff', padding: 0 }}>
+				<Header style={{ background: "#fff", padding: 0 }}>
 					<Icon
-						className='da__dashboard__trigger'
-						type={stateCollapsed.collapsed ? 'menu-unfold' : 'menu-fold'}
+						className="da__dashboard__trigger"
+						type={stateCollapsed.collapsed ? "menu-unfold" : "menu-fold"}
 						onClick={toggle}
 					/>
 				</Header>
 				<Content
 					style={{
-						margin: '0 16px'
+						margin: "0 16px"
 					}}
 				>
-					<Breadcrumb style={{ margin: '16px 0' }}>
+					<Breadcrumb style={{ margin: "16px 0" }}>
 						<Breadcrumb.Item>User</Breadcrumb.Item>
 						<Breadcrumb.Item>Bill</Breadcrumb.Item>
 					</Breadcrumb>
-					<div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+					<div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
 						Here goes the content
 					</div>
 				</Content>
-				<Footer style={{ textAlign: 'center' }}>
+				<Footer style={{ textAlign: "center" }}>
 					DA Admin Theme ©2019 Created by DA
 				</Footer>
 			</Layout>
